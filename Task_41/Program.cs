104815edb0130913205c903ece57736d97ae55d0
Dima Mynka <mdv8804@gmail.com>
2023-02-15 10:51:20 +0200

    metod Split

---
+++ Task_41/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.//
+
+//0, 7, 8, -2, -2 -> 2//
+
+//1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+int[] n = Console.ReadLine().Split(' ').Select(e => Convert.ToInt32(e)).ToArray();
+
+int result = Sum0(n);
+System.Console.WriteLine(result);
+
+int Sum0(int[] array)
+{
+    int res = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0)
+        {
+            res = res + 1;
+        }
+
+    }
+    return res;
+}
