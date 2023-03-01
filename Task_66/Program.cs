833bcbba7198baf029317f68eab728112f2363af
Dima Mynka <mdv8804@gmail.com>
2023-03-01 17:27:10 +0200

    sumgap

---
+++ Task_66/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+System.Console.WriteLine("M");
+int m = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("N");
+int n = int.Parse(Console.ReadLine());
+int res = SumGap(m,n);
+System.Console.WriteLine(res);
+
+
+int SumGap(int a,int b)
+{
+    int sum = 0;
+    if(b == 1)
+    { 
+    return a;
+    }
+     else
+     {
+      sum =  b + SumGap(a,b - 1);
+      return sum;
+     }
+    
+}
+
